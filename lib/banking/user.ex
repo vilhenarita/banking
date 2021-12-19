b94f6fba2751bc3ad54a20692de61a41af003303
rita.vilhena <ritinha.vilhena@gmail.com>
2021-12-18 22:38:30 -0300

    criação do changeset de users e algumas validações

--- lib/banking/user.ex
+++ lib/banking/user.ex
@@ -1,5 +1,23 @@
 defmodule Banking.User do
   use Ecto.Schema
+  import Ecto.Changeset
+
+  #campos que podem ser alterados
+  @changeable_fields [
+    :name,
+    :cpf,
+    :email,
+    :password_hash,
+    :balance
+  ]
+
+  #campos obrigatórios
+  @required_fields [
+    :name,
+    :cpf,
+    :email,
+    :password_hash
+  ]
 
   #Configurando o tipo de chave primária e geração automática
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -12,6 +30,17 @@ defmodule Banking.User do
     field :balance,   :decimal
 
       timestamps()
+  end
+
+  def changeset(%{} = params) do
+    %__MODULE__{}
+    |> cast(params,@changeable_fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:password_hash,min: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
+    |> unique_constraint(:cpf)
   end
 
 end
